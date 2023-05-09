913eaeafd9fd509dd57c86c9448fc11ff9662e72
shiye7 <jcancajas07@gmail.com>
2023-05-10 01:17:46 +0800

    Fixing Credits Modal

--- src/pages/private/credits/credit-table.php
+++ src/pages/private/credits/credit-table.php
@@ -17,14 +17,40 @@
         <td>1</td>
         <td class="actions">
           <div class="add-modal d-inline">
-            <div class="modal fade" id="addModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal fade p-0" id="addModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
               <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
-                  <div class="modal-header">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  <div class="modal-header bg-dark">
+                    <span class="fs-5 text-white">
+                      Modal Title Here
+                    </span>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
-                    ADD SOMETHING HERE
+                    <span class="fs-5 fw-semibold me-2">
+                      <?php echo User::currentUser()->getFullName(); ?>
+                    </span>
+                    <span class="fs-7">
+                      (Available credits here: )
+                    </span>
+                    <div class="row my-3">
+                      <label for="addCredits" class="col-sm-3 col-form-label">Add Credits</label>
+                      <div class="col-sm-9">
+                        <input type="text" class="form-control" id="addCredits">
+                      </div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                      <div>
+                        <button type="button" class="btn btn-success me-2">
+                          <span>SAVE</span>
+                        </button>
+                      </div>
+                      <div>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" aria-label="Close">
+                          CANCEL
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -38,11 +64,37 @@
             <div class="modal fade" id="deductModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
               <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
-                  <div class="modal-header">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  <div class="modal-header bg-dark">
+                    <span class="fs-5 text-white">
+                      Modal Title Here
+                    </span>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
-                    DEDUCT SOMETHING HERE
+                    <span class="fs-5 fw-semibold me-2">
+                      <?php echo User::currentUser()->getFullName(); ?>
+                    </span>
+                    <span class="fs-7">
+                      (Existing credits here: )
+                    </span>
+                    <div class="row my-3">
+                      <label for="deductCredits" class="col-sm-4 col-form-label">Deduct Credits</label>
+                      <div class="col-sm-8">
+                        <input type="text" class="form-control" id="deductCredits">
+                      </div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                      <div>
+                        <button type="button" class="btn btn-success me-2">
+                          <span>SAVE</span>
+                        </button>
+                      </div>
+                      <div>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" aria-label="Close">
+                          CANCEL
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
